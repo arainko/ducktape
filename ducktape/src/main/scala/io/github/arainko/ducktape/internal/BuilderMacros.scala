dbd68b8b685ba4088bd4f215f14ba14b2e88479d
arainko <aleksander.rainko99@gmail.com>
2021-11-21 21:59:30 +0100

    added safe field selector

--- ducktape/src/main/scala/io/github/arainko/ducktape/internal/BuilderMacros.scala
+++ ducktape/src/main/scala/io/github/arainko/ducktape/internal/BuilderMacros.scala
@@ -3,27 +3,63 @@ package io.github.arainko.ducktape.internal
 import scala.quoted.*
 import io.github.arainko.ducktape.Field
 import scala.deriving.Mirror
-
+// import io.github.arainko.ducktape.internal.BuilderMacros.FieldSelect
 
 object BuilderMacros {
-  transparent inline def droppedWithLambda[Fields <: Tuple] =
-    ${ droppedWithLambdaImpl[Fields] }
+  transparent inline def droppedWithLambda[
+    From,
+    FieldType,
+    Fields <: Tuple
+  ](inline lambda: From => FieldType) =
+    ${ droppedWithLambdaImpl[From, FieldType, Fields]('lambda) }
 
-  def droppedWithLambdaImpl[Fields <: Tuple: Type](using Quotes) = 
-    BuilderMacros().fromInlinedLambda[Fields] 
-    
+  def droppedWithLambdaImpl[
+    From: Type,
+    FieldType: Type,
+    Fields <: Tuple: Type
+  ](lambda: Expr[From => FieldType])(using Quotes) =
+    BuilderMacros().selectFromInlinedLambda[From, FieldType, Fields](lambda)
+
 }
 
-class BuilderMacros(using Quotes) {
+class BuilderMacros(using val quotes: Quotes) {
   import quotes.reflect.*
 
-  def fromInlinedLambda[Fields <: Tuple: Type] = {
-    val cons = ConstantType(StringConstant("costam")).asType
-    val droppedType = cons match {
+  def selectFromInlinedLambda[
+    From,
+    FieldType,
+    Fields <: Tuple: Type
+  ](lambda: Expr[From => FieldType]) = {
+    given Printer[Tree] = Printer.TreeStructure
+    println(lambda.asTerm.show)
+    val selectedFieldName = lambda.asTerm match {
+      case FieldSelector(fieldName) => fieldName
+      case _                        => report.errorAndAbort("Not a field selector!")
+    }
+    val fieldNameType = ConstantType(StringConstant(selectedFieldName)).asType
+    val droppedType = fieldNameType match {
       case '[field] => Type.of[Field.DropByLabel[field, Fields]]
     }
     droppedType match {
       case '[dropped] => '{ ??? : dropped }
     }
   }
+
+  object FieldSelector:
+    private object SelectorLambda:
+      def unapply(arg: Term): Option[(List[ValDef], Term)] =
+        arg match {
+          case Inlined(_, _, Lambda(vals, term)) => Some((vals, term))
+          case Inlined(_, _, nested)             => SelectorLambda.unapply(nested)
+          case t                                 => None
+        }
+    end SelectorLambda
+
+    def unapply(arg: Term) =
+      arg match {
+        case SelectorLambda(_, Select(Ident(_), fieldName)) => Some(fieldName)
+        case _                                              => None
+      }
+  end FieldSelector
+
 }
--- ducktape/src/main/scala/io/github/arainko/ducktape/internal/BuilderMacrosUsage.scala
+++ ducktape/src/main/scala/io/github/arainko/ducktape/internal/BuilderMacrosUsage.scala
@@ -8,6 +8,10 @@ final case class Costam(costam: Int, field: String, field1: Int)
 
 object BuilderMacrosUsage {
   val mirror = summon[Mirror.Of[Costam]]
-  val cos: Field["field", String] *: Field["field1", Int] *: EmptyTuple = 
-    BuilderMacros.droppedWithLambda[Field.FromLabelsAndTypes[mirror.MirroredElemLabels, mirror.MirroredElemTypes]]
+  val cos/*: Field["field", String] *: Field["field1", Int] *: EmptyTuple*/ = 
+    BuilderMacros.droppedWithLambda[
+    Costam,
+    Int,
+    Field.FromLabelsAndTypes[mirror.MirroredElemLabels, mirror.MirroredElemTypes]
+    ](_.costam)
 }
