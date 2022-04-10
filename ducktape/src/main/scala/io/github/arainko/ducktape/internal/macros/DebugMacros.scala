ca6c4557f22d65ca7a0ffbdcbd12775ef114ba0a
Aleksander Rainko <aleksander.rainko99@gmail.com>
2022-04-10 03:08:26 +0200

    initial work

--- ducktape/src/main/scala/io/github/arainko/ducktape/internal/macros/DebugMacros.scala
+++ ducktape/src/main/scala/io/github/arainko/ducktape/internal/macros/DebugMacros.scala
@@ -1,6 +1,7 @@
 package io.github.arainko.ducktape.internal.macros
 
 import scala.quoted.*
+import io.github.arainko.ducktape.*
 
 object DebugMacros {
   inline def structure[A](inline value: A) = ${ structureMacro('value) }
@@ -18,10 +19,51 @@ object DebugMacros {
 
   def codeMacro[A: Type](value: Expr[A])(using Quotes) = {
     import quotes.reflect.*
+
     val struct = Printer.TreeShortCode.show(value.asTerm)
     '{
       println(${ Expr(struct) })
       $value
+    }
+  }
+
+  transparent inline def methodParams(inline func: Any) = ${ methodParamsMacro('func) }
+
+  def methodParamsMacro(function: Expr[Any])(using Quotes) = {
+    import quotes.reflect.*
+
+    function.asTerm match {
+      case b @ Inlined(_, _, l @ Lambda(vals, body)) =>
+        vals.map(_.tpt.tpe)
+
+        Select.unique(b, "apply").appliedTo(Literal(IntConstant(1))).asExpr
+      // b.appliedTo(Literal(IntConstant(1))).asExpr
+      // Expr(vals.map(v => v.name -> v.tpt.tpe.show).map(_.toString))
+    }
+  }
+
+  transparent inline def functionMirror[A] = ${ functionMirrorMacro[A] }
+
+  def functionMirrorMacro[A: Type](using Quotes) = {
+    import quotes.reflect.*
+    val tpe = TypeRepr.of[A]
+    if (!tpe.isFunctionType) report.errorAndAbort("NOT A FUNCTION!")
+    val cons = TypeRepr.of[*:]
+    val AppliedType(_, tpeArgs) = tpe
+
+    val returnTpe = tpeArgs.last
+    val args = tpeArgs.init
+
+    val argsTuple = args.foldRight(TypeRepr.of[EmptyTuple])((curr, acc) => cons.appliedTo(curr :: acc :: Nil))
+
+    (returnTpe.asType -> argsTuple.asType) match {
+      case ('[ret], '[args]) =>
+        '{
+          new FunctionMirror[A] {
+            type Return = ret
+            type Args = args
+          }
+        }
     }
   }
 }
--- ducktape/src/main/scala/io/github/arainko/ducktape/syntax.scala
+++ ducktape/src/main/scala/io/github/arainko/ducktape/syntax.scala
@@ -1,9 +1,49 @@
 package io.github.arainko.ducktape
 
 import io.github.arainko.ducktape.Builder.Applied
+import io.github.arainko.ducktape.internal.macros.DebugMacros
+import scala.runtime.FunctionXXL
+import javax.swing.DebugGraphics
 
 extension [From](value: From) {
   def into[To]: Applied[From, To, EmptyTuple] = Builder.applied[From, To](value)
 
   def to[To](using Transformer[From, To]): To = Transformer[From, To].transform(value)
+}
+
+final case class Costam(int: Int, value: String)
+
+trait FunctionMirror[F] {
+  type Args
+  type Return
+}
+
+object FunctionMirror {
+  transparent inline def values[F] = DebugMacros.functionMirror[F]
+}
+
+
+@main def run = {
+
+  DebugMacros.structure {
+    new Function1[Int, Int] {
+      def apply(int: Int): Int = int
+    }
+  }
+
+  // DebugMacros.code {
+
+  // }
+
+
+
+  // summon[cos.sReturn =:= Int]
+  // summon[cos.Args =:= (Int, String)]
+
+  // val res: Costam = cos.tupled((1, "asd"))
+
+
+  // println(res)
+  // val res = DebugMacros.methodParams(Costam.apply)
+  // println(res)
 }
