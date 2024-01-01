1f2dfb5594097718f1d8e47d051b66af03be1689
Aleksander Rainko <aleksander.rainko99@gmail.com>
2024-01-02 00:40:43 +0100

    initial work on FalliblePlanInterpreter

--- ducktape/src/main/scala/io/github/arainko/ducktape/internal/FalliblePlanInterpreter.scala
+++ ducktape/src/main/scala/io/github/arainko/ducktape/internal/FalliblePlanInterpreter.scala
@@ -4,5 +4,57 @@ import scala.quoted.*
 import io.github.arainko.ducktape.Mode
 
 object FalliblePlanInterpreter {
-  def run[F[+x], A](plan: Plan[Nothing], sourceValue: Expr[A], mode: Expr[Mode.Accumulating[F]])(using Quotes): Expr[F[Any]] = ???
+  def run[F[+x], A](
+    plan: Plan[Nothing],
+    sourceValue: Expr[A],
+    mode: Expr[Mode.Accumulating[F]]
+  )(using Quotes): Expr[F[Any]] = ???
+
+  private def recurse[F[+x], A: Type](
+    plan: Plan[Nothing],
+    value: Expr[Any],
+    F: Expr[Mode.Accumulating[F]]
+  )(using toplevelValue: Expr[A])(using Quotes): Expr[F[Any]] = {
+    plan match {
+      case Plan.Upcast(_, _, _, _) => '{ $F.pure($value) }
+
+      case Plan.Configured(_, _, _, _, config) => ???
+
+      case Plan.BetweenProducts(source, dest, _, _, fieldPlans) => ???
+
+      case Plan.BetweenCoproducts(source, dest, _, _, casePlans) => ???
+
+      case Plan.BetweenProductFunction(source, dest, _, _, argPlans) => ???
+
+      case Plan.BetweenOptions(source, dest, _, _, plan) =>
+        (source.paramStruct.tpe, dest.paramStruct.tpe) match {
+          case '[src] -> '[dest] =>
+            val source = value.asExprOf[Option[src]]
+            '{
+              $source match
+                case None        => $F.pure(None)
+                case Some(value) => $F.map(${ recurse(plan, 'value, F) }, Some.apply)
+            }
+        }
+
+      case Plan.BetweenNonOptionOption(source, dest, _, _, plan) =>
+        source.tpe match {
+          case '[src] =>
+            val source = value.asExprOf[src]
+            '{ $F.map(${ recurse(plan, source, F) }, Some.apply) }
+        }
+
+      case Plan.BetweenCollections(source, dest, _, _, plan) => ???
+
+      case Plan.BetweenSingletons(source, dest, _, _) => '{ $F.pure(${ dest.value }) }
+
+      case Plan.BetweenWrappedUnwrapped(source, dest, _, _, fieldName) => ???
+
+      case Plan.BetweenUnwrappedWrapped(source, dest, _, _) => ???
+
+      case Plan.UserDefined(source, dest, _, _, transformer) => ???
+
+      case Plan.Derived(source, dest, _, _, transformer) => ???
+    }
+  }
 }
