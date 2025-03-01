4ce083be13ca68e2e53d3eda147aedfda75ddd6d
arainko <aleksander.rainko99@gmail.com>
2025-03-01 07:39:01 +0100

    more test for local flags

--- ducktape/src/test/scala/io/github/arainko/ducktape/total/FlagSuite.scala
+++ ducktape/src/test/scala/io/github/arainko/ducktape/total/FlagSuite.scala
@@ -71,6 +71,40 @@ class FlagSuite extends DucktapeSuite {
     )
   }
 
+  test("source function arg renames work") {
+    case class Source(int: Int, str: String)
+    case class Dest(INT_ADDITION: Int, STR: String)
+
+    val source = Source(1, "1")
+    val expected = Dest(1, "1")
+
+    assertEachEquals(
+      source.intoVia(Dest.apply).transform(
+        Field.modifySourceNames(_.toUpperCase.rename("INT", "INT_ADDITION"))
+      ),
+      Transformer.defineVia[Source](Dest.apply).build(
+        Field.modifySourceNames(_.toUpperCase.rename("INT", "INT_ADDITION"))
+      ).transform(source)
+    )(expected)
+  }
+
+  test("dest function arg renames work") {
+    case class Source(int: Int, str: String)
+    case class Dest(INT_ADDITION: Int, STR: String)
+
+    val source = Source(1, "1")
+    val expected = Dest(1, "1")
+
+    assertEachEquals(
+      source.intoVia(Dest.apply).transform(
+        Field.modifyDestNames(_.toLowerCase.replace("_addition", ""))
+      ),
+      Transformer.defineVia[Source](Dest.apply).build(
+        Field.modifyDestNames(_.toLowerCase.replace("_addition", ""))
+      ).transform(source)
+    )(expected)
+  }
+
   test("source case object renames work") {
     case object Source_Frank_Beltrame
     case object SOURCE
@@ -89,117 +123,330 @@ class FlagSuite extends DucktapeSuite {
     )
   }
 
-  test("dest local flag covers the selected case class and nothing else") {
-    case class Source(int: Int, str: String, level1: SourceLevel1)
-    case class SourceLevel1(INT: Int, STR: String, LEVEL2: SourceLevel2)
-    case class SourceLevel2(int: Int, str: String)
-
-    case class Dest(int: Int, str: String, level1: DestLevel1)
-    case class DestLevel1(int: Int, str: String, level2: DestLevel2)
-    case class DestLevel2(int: Int, str: String)
-
-    val source = Source(1, "1", SourceLevel1(2, "2", SourceLevel2(3, "3")))
-    val expected = Dest(1, "1", DestLevel1(2, "2", DestLevel2(3, "3")))
-
-    assertTransformsConfigured(source, expected)(
-      Field.modifyDestNames(_.toUpperCase).local(_.level1)
-    )
-  }
-
-  test("source local flag covers the selected case class and nothing else") {
-    case class Source(int: Int, str: String, level1: SourceLevel1)
-    case class SourceLevel1(int: Int, str: String, level2: SourceLevel2)
-    case class SourceLevel2(int: Int, str: String)
-
-    case class Dest(int: Int, str: String, level1: DestLevel1)
-    case class DestLevel1(INT: Int, STR: String, LEVEL2: DestLevel2)
-    case class DestLevel2(int: Int, str: String)
-
-    val source = Source(1, "1", SourceLevel1(2, "2", SourceLevel2(3, "3")))
-    val expected = Dest(1, "1", DestLevel1(2, "2", DestLevel2(3, "3")))
-
-    assertTransformsConfigured(source, expected)(
-      Field.modifySourceNames(_.toUpperCase).local(_.level1)
-    )
-  }
-
-  test("source local flag covers the selected subtype of an enum and nothing else") {
-    case class SourceLevel1(int: Int)
-    case class DestLevel1(int: Int)
-
-    enum Source {
-      case One(int: Int, str: String)
-      case Two(int: Int, str: String, level1: SourceLevel1)
-      case Three(int: Int, str: String)
+  test("dest flag carries through BetweenOptions") {
+    case class Source(int: Int, str: String, level1: Option[Source.Level1])
+    object Source {
+      case class Level1(int: Int, level2: Level2)
+      case class Level2(str: String)
     }
 
-    enum Dest {
-      case One(int: Int, str: String)
-      case Two(INT: Int, STR: String, LEVEL1: DestLevel1)
-      case Three(int: Int, str: String)
+
+    case class Dest(int: Int, str: String, level1: Option[Dest.Level1])
+    object Dest {
+      case class Level1(int: Int, level2: Level2)
+      case class Level2(STR: String)
     }
 
     assertTransformsConfigured(
-      Source.Two(2, "2", SourceLevel1(1)),
-      Dest.Two(2, "2", DestLevel1(1))
+      Source(1, "1", Some(Source.Level1(2, Source.Level2("3")))),
+      Dest(1, "1", Some(Dest.Level1(2, Dest.Level2("3"))))
     )(
-      Field.modifySourceNames(_.toUpperCase).local(_.at[Source.Two])
+      Field.modifyDestNames(_.toLowerCase).local(_.level1.element.level2),
     )
   }
 
-  test("source local flag covers all subtypes of an enum and nothing else") {
-    case class Source(int: Int, level1: SourceEnum)
-    case class Dest(int: Int, level1: DestEnum)
-
-    enum SourceEnum {
-      case One(int: Int, str: String)
-      case Two(int: Int, str: String, level1: SourceLevel1)
-      case Three(int: Int, str: String)
+  test("source flag carries through BetweenOptions") {
+    case class Source(int: Int, str: String, level1: Option[Source.Level1])
+    object Source {
+      case class Level1(int: Int, level2: Level2)
+      case class Level2(STR: String)
     }
 
-    enum DestEnum {
-      case One(INT: Int, STR: String)
-      case Two(INT: Int, STR: String, LEVEL1: DestLevel1)
-      case Three(INT: Int, STR: String)
-    }
 
-    case class SourceLevel1(int: Int)
-    case class DestLevel1(int: Int)
+    case class Dest(int: Int, str: String, level1: Option[Dest.Level1])
+    object Dest {
+      case class Level1(int: Int, level2: Level2)
+      case class Level2(str: String)
+    }
 
     assertTransformsConfigured(
-      Source(1,SourceEnum.Two(2, "2", SourceLevel1(3))),
-      Dest(1, DestEnum.Two(2, "2", DestLevel1(3)))
+      Source(1, "1", Some(Source.Level1(2, Source.Level2("3")))),
+      Dest(1, "1", Some(Dest.Level1(2, Dest.Level2("3"))))
     )(
-      Field.modifySourceNames(_.toUpperCase).local(_.level1)
+      Field.modifySourceNames(_.toLowerCase).local(_.level1.element.level2),
     )
   }
 
-  test("source local flag covers all subtypes of an enum and nothing else") {
-    case class Source(int: Int, level1: SourceEnum)
-    case class Dest(int: Int, level1: DestEnum)
-
-    enum DestEnum {
-      case One(int: Int, str: String)
-      case Two(int: Int, str: String, level1: DestLevel1)
-      case Three(int: Int, str: String)
+  test("dest flag carries through NonOptionOption") {
+    case class Source(int: Int, str: String, level1: Source.Level1)
+    object Source {
+      case class Level1(int: Int, level2: Level2)
+      case class Level2(str: String)
     }
 
-    enum SourceEnum {
-      case One(INT: Int, STR: String)
-      case Two(INT: Int, STR: String, LEVEL1: SourceLevel1)
-      case Three(INT: Int, STR: String)
-    }
 
-    case class SourceLevel1(int: Int)
-    case class DestLevel1(int: Int)
+    case class Dest(int: Int, str: String, level1: Option[Dest.Level1])
+    object Dest {
+      case class Level1(int: Int, level2: Level2)
+      case class Level2(STR: String)
+    }
 
     assertTransformsConfigured(
-      Source(1,SourceEnum.Two(2, "2", SourceLevel1(3))),
-      Dest(1, DestEnum.Two(2, "2", DestLevel1(3)))
+      Source(1, "1", Source.Level1(2, Source.Level2("3"))),
+      Dest(1, "1", Some(Dest.Level1(2, Dest.Level2("3"))))
     )(
-      Field.modifyDestNames(_.toUpperCase).local(_.level1)
+      Field.modifyDestNames(_.toLowerCase).local(_.level1.element.level2),
     )
   }
 
+  test("source flag carries through NonOptionOption") {
+    case class Source(int: Int, str: String, level1: Source.Level1)
+    object Source {
+      case class Level1(int: Int, level2: Level2)
+      case class Level2(STR: String)
+    }
 
+
+    case class Dest(int: Int, str: String, level1: Option[Dest.Level1])
+    object Dest {
+      case class Level1(int: Int, level2: Level2)
+      case class Level2(str: String)
+    }
+
+    assertTransformsConfigured(
+      Source(1, "1", Source.Level1(2, Source.Level2("3"))),
+      Dest(1, "1", Some(Dest.Level1(2, Dest.Level2("3"))))
+    )(
+      Field.modifySourceNames(_.toLowerCase).local(_.level1.level2),
+    )
+  }
+
+  test("dest flag carries through BetweenCollections") {
+    case class Source(int: Int, str: String, level1: List[Source.Level1])
+    object Source {
+      case class Level1(int: Int, level2: Level2)
+      case class Level2(str: String)
+    }
+
+
+    case class Dest(int: Int, str: String, level1: Vector[Dest.Level1])
+    object Dest {
+      case class Level1(int: Int, level2: Level2)
+      case class Level2(STR: String)
+    }
+
+    assertTransformsConfigured(
+      Source(1, "1", List(Source.Level1(2, Source.Level2("3")))),
+      Dest(1, "1", Vector(Dest.Level1(2, Dest.Level2("3"))))
+    )(
+      Field.modifyDestNames(_.toLowerCase).local(_.level1.element.level2),
+    )
+  }
+
+  test("source flag carries through BetweenCollections") {
+    case class Source(int: Int, str: String, level1: List[Source.Level1])
+    object Source {
+      case class Level1(int: Int, level2: Level2)
+      case class Level2(STR: String)
+    }
+
+    case class Dest(int: Int, str: String, level1: Vector[Dest.Level1])
+    object Dest {
+      case class Level1(int: Int, level2: Level2)
+      case class Level2(str: String)
+    }
+
+    assertTransformsConfigured(
+      Source(1, "1", List(Source.Level1(2, Source.Level2("3")))),
+      Dest(1, "1", Vector(Dest.Level1(2, Dest.Level2("3"))))
+    )(
+      Field.modifySourceNames(_.toLowerCase).local(_.level1.element.level2),
+    )
+  }
+
+  test("dest flag carries through ProductTuple") {
+    case class Source(int: Int, str: String, level1: List[Source.Level1])
+    object Source {
+      case class Level1(int: Int, level2: Level2)
+      case class Level2(str: String)
+    }
+
+
+    case class Dest(int: Int, str: String, level1: Vector[(Int, Dest.Level2)])
+    object Dest {
+      case class Level2(STR: String)
+    }
+
+    assertTransformsConfigured(
+      Source(1, "1", List(Source.Level1(2, Source.Level2("3")))),
+      Dest(1, "1", Vector((2, Dest.Level2("3"))))
+    )(
+      Field.modifyDestNames(_.toLowerCase).local(_.level1.element._2),
+    )
+  }
+
+  test("source flag carries through ProductTuple") {
+    case class Source(int: Int, str: String, level1: List[Source.Level1])
+    object Source {
+      case class Level1(int: Int, level2: Level2)
+      case class Level2(STR: String)
+    }
+
+
+    case class Dest(int: Int, str: String, level1: Vector[(Int, Dest.Level2)])
+    object Dest {
+      case class Level2(str: String)
+    }
+
+    assertTransformsConfigured(
+      Source(1, "1", List(Source.Level1(2, Source.Level2("3")))),
+      Dest(1, "1", Vector((2, Dest.Level2("3"))))
+    )(
+      Field.modifySourceNames(_.toLowerCase).local(_.level1.element.level2),
+    )
+  }
+
+  test("source flag carries through TupleProduct") {
+    case class Source(int: Int, str: String, level1: List[(Int, Source.Level2)])
+    object Source {
+      case class Level2(str: String)
+    }
+
+    case class Dest(int: Int, str: String, level1: Vector[Dest.Level1])
+    object Dest {
+      case class Level1(int: Int, level2: Level2)
+      case class Level2(STR: String)
+    }
+
+    assertTransformsConfigured(
+      Source(1, "1", List((2, Source.Level2("3")))),
+      Dest(1, "1", Vector(Dest.Level1(2, Dest.Level2("3"))))
+    )(
+      Field.modifySourceNames(_.toUpperCase).local(_.level1.element._2),
+    )
+  }
+
+  test("dest flag carries through TupleProduct") {
+    case class Source(int: Int, str: String, level1: List[(Int, Source.Level2)])
+    object Source {
+      case class Level2(str: String)
+    }
+
+    case class Dest(int: Int, str: String, level1: Vector[Dest.Level1])
+    object Dest {
+      case class Level1(int: Int, level2: Level2)
+      case class Level2(STR: String)
+    }
+
+    assertTransformsConfigured(
+      Source(1, "1", List((2, Source.Level2("3")))),
+      Dest(1, "1", Vector(Dest.Level1(2, Dest.Level2("3"))))
+    )(
+      Field.modifyDestNames(_.toLowerCase).local(_.level1.element.level2),
+    )
+  }
+
+  test("source flag carries through BetweenTuples") {
+    case class Source(int: Int, str: String, level1: List[(Int, Source.Level2)])
+    object Source {
+      case class Level2(str: String)
+    }
+
+    case class Dest(int: Int, str: String, level1: Vector[(Int, Dest.Level2)])
+    object Dest {
+      case class Level2(STR: String)
+    }
+
+    assertTransformsConfigured(
+      Source(1, "1", List((2, Source.Level2("3")))),
+      Dest(1, "1", Vector((2, Dest.Level2("3"))))
+    )(
+      Field.modifySourceNames(_.toUpperCase).local(_.level1.element._2),
+    )
+  }
+
+  test("dest flag carries through BetweenTuples") {
+    case class Source(int: Int, str: String, level1: List[(Int, Source.Level2)])
+    object Source {
+      case class Level2(str: String)
+    }
+
+    case class Dest(int: Int, str: String, level1: Vector[Option[(Int, Dest.Level2)]])
+    object Dest {
+      case class Level2(STR: String)
+    }
+
+    assertTransformsConfigured(
+      Source(1, "1", List((2, Source.Level2("3")))),
+      Dest(1, "1", Vector(Some((2, Dest.Level2("3")))))
+    )(
+      Field.modifyDestNames(_.toLowerCase).local(_.level1.element.element._2),
+    )
+  }
+
+  test("source flag carries through BetweenFallibleNonFallible") {
+    case class Source(int: Int, str: String, level1: Option[Source.Level1])
+    object Source {
+      case class Level1(int: Int, level2: Level2)
+      case class Level2(STR: String)
+    }
+
+    case class Dest(int: Int, str: String, level1: Dest.Level1)
+    object Dest {
+      case class Level1(int: Int, level2: Level2)
+      case class Level2(str: String)
+    }
+
+    Mode.FailFast.option.locally {
+      assertTransformsFallibleConfigured(
+        Source(1, "1", Some(Source.Level1(2, Source.Level2("3")))),
+        Some(Dest(1, "1", Dest.Level1(2, Dest.Level2("3")))),
+      )(
+        Field.modifySourceNames(_.toLowerCase).local(_.level1.element.level2),
+      )
+    }
+  }
+
+  //TODO: Broken! - however rethink BetweenFallibleNonFallible and BetweenFallibles...
+  // test("dest flag carries through BetweenFallibleNonFallible") {
+  //   case class Source(int: Int, str: String, level1: Option[Source.Level1])
+  //   object Source {
+  //     case class Level1(int: Int, level2: Level2)
+  //     case class Level2(STR: String)
+  //   }
+
+  //   case class Dest(int: Int, str: String, level1: Dest.Level1)
+  //   object Dest {
+  //     case class Level1(int: Int, level2: Level2)
+  //     case class Level2(str: String)
+  //   }
+
+  //   Mode.FailFast.option.locally {
+  //     assertTransformsFallibleConfigured(
+  //       Source(1, "1", Some(Source.Level1(2, Source.Level2("3")))),
+  //       Some(Dest(1, "1", Dest.Level1(2, Dest.Level2("3")))),
+  //     )(
+  //       Field.modifyDestNames(_.toUpperCase).regional(_.level1.level2),
+  //     )
+  //   }
+  // }
+
+  // test("source flag carries through BetweenFallibleNonFallible") {
+  //   case class Source(int: Int, str: String, level1: Either[String, Source.Level1])
+  //   object Source {
+  //     case class Level1(int: Int, level2: Level2)
+  //     case class Level2(STR: String)
+  //   }
+
+  //   case class Dest(int: Int, str: String, level1: Either[String, Dest.Level1])
+  //   object Dest {
+  //     case class Level1(int: Int, level2: Level2)
+  //     case class Level2(str: String)
+  //   }
+
+  //   Mode.FailFast.either[String].locally {
+  //     assertTransformsFallibleConfigured(
+  //       Source(1, "1", Right(Source.Level1(2, Source.Level2("3")))),
+  //       Right(Dest(1, "1", Right(Dest.Level1(2, Dest.Level2("3"))))),
+  //     )(
+  //       Field.modifySourceNames(_.toLowerCase).local(_.level1.element.level2),
+  //     )
+  //   }
+  // }
+
+  //todo: tuple-function, fallibles
+  //todo: priority overwrites of flags
+  //todo: name ambiguities
+  //todo: regional flags (copy-paste of local flag tests)
+  //todo: type specific flags
 }
