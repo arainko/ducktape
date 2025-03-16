963088f3b4f660d3b79eaaec7280a0b58af11bde
arainko <aleksander.rainko99@gmail.com>
2025-03-16 18:31:26 +0100

    add scaladoc to Regional, Local and TypeSpecific

--- ducktape/src/main/scala/io/github/arainko/ducktape/Regional.scala
+++ ducktape/src/main/scala/io/github/arainko/ducktape/Regional.scala
@@ -6,6 +6,32 @@ type Regional[A]
 
 object Regional {
   extension [F[a, b] <: (Case[a, b] | Field[a, b]), A, B, C](self: F[A, B] & Regional[C]) {
+
+    /**
+     * Constrains a config option to a certain region (i.e. the option will apply to the transformations 'underneath' the selected field/case):
+     *
+     * {{{
+     * case class Person(name: String, age: Int, info: Person.Info)
+     * object Person {
+     *   case class Info(accountNo: String, email: String)
+     * }
+     *
+     * case class ReshuffledPerson(age: Int, name: String, extra: Option[String], info: ReshuffledPerson.Info)
+     *
+     * object ReshuffledPerson {
+     *   case class Info(accountNo: String, email: String, extraInfo: Option[String])
+     * }
+     *
+     * val person: Person = Person("Name", 26, Person.Info("123", "email@example.com"))
+     * person
+     *  .into[ReshuffledPerson]
+     *  .transform(
+     *    Field.const(_.extra, Some("filled out with const since fallback won't be used here now"))
+     *    Field.fallbackToNone.regional(_.info)
+     *  )
+     * // ReshuffledPerson(26, "Name", Some("filled out with const since fallback won't be used here now"), ReshuffledPerson.Info("123", "email@example.com", None))
+     * }}}
+     */
     @compileTimeOnly(".regional is only usable as field configuration for transformations")
     def regional[DestFieldTpe](selector: Selector ?=> C => DestFieldTpe): F[A, B] = ???
   }
@@ -15,6 +41,27 @@ type Local[A]
 
 object Local {
   extension [F[a, b] <: (Case[a, b] | Field[a, b]), A, B, C](self: F[A, B] & Local[C]) {
+
+    /**
+     * Constrains a config option to a certain local region (i.e. to a case class/children of an enum 'underneath' the selected field/case):
+     *
+     * {{{
+     * case class Source(int: Int, str: String, level1: SourceLevel1)
+     * case class SourceLevel1(INT: Int, STR: String, LEVEL2: SourceLevel2)
+     * case class SourceLevel2(int: Int, str: String)
+     *
+     * case class Dest(int: Int, str: String, level1: DestLevel1)
+     * case class DestLevel1(int: Int, str: String, level2: DestLevel2)
+     * case class DestLevel2(int: Int, str: String)
+     *
+     * val source = Source(1, "1", SourceLevel1(2, "2", SourceLevel2(3, "3")))
+     * 
+     * source
+     *  .into[Dest]
+     *  .transform(Field.modifyDestNames(_.toUpperCase).local(_.level1)) // <-- we use `.local` to only modify names under `Dest.level1` and not anywhere else
+     * // Dest(1, "1", DestLevel1(2, "2", DestLevel2(3, "3")))
+     * }}}
+     */
     @compileTimeOnly(".local is only usable as field configuration for transformations")
     def local[DestFieldTpe](selector: Selector ?=> C => DestFieldTpe): F[A, B] = ???
   }
@@ -24,6 +71,26 @@ type TypeSpecific
 
 object TypeSpecific {
   extension [F[a, b] <: (Case[a, b] | Field[a, b]), A, B](self: F[A, B] & TypeSpecific) {
+    /**
+     * Constrains a config option to a subtypes of the selected type:
+     *
+     * {{{
+     * case class Source(int: Int, str: String, level1: SourceLevel1)
+     * case class SourceLevel1(INT: Int, STR: String, LEVEL2: SourceLevel2)
+     * case class SourceLevel2(int: Int, str: String)
+     *
+     * case class Dest(int: Int, str: String, level1: DestLevel1)
+     * case class DestLevel1(int: Int, str: String, level2: DestLevel2)
+     * case class DestLevel2(int: Int, str: String)
+     *
+     * val source = Source(1, "1", SourceLevel1(2, "2", SourceLevel2(3, "3")))
+     * 
+     * source
+     *  .into[Dest]
+     *  .transform(Field.modifyDestNames(_.toUpperCase).typeSpecific[DestLevel1]) // <-- we use `.typeSpecifc` to only modify names for `DestLevel1` and not anywhere else
+     * // Dest(1, "1", DestLevel1(2, "2", DestLevel2(3, "3")))
+     * }}}
+     */
     @compileTimeOnly(".typeSpecific is only usable as field configuration for transformations")
     def typeSpecific[Tpe]: F[A, B] = ???
   }
--- ducktape/src/main/scala/io/github/arainko/ducktape/Transformer.scala
+++ ducktape/src/main/scala/io/github/arainko/ducktape/Transformer.scala
@@ -5,7 +5,7 @@ import io.github.arainko.ducktape.Transformer.Derived.FromFunction
 import io.github.arainko.ducktape.internal.{ FallibleTransformations, TotalTransformations }
 
 /**
-  * A `Transformer[Source, Dest]` describes the total transformation from `Source` to `Dest`.
+  * A `Transformer[Source, Dest]` describes a total transformation from `Source` to `Dest`.
   * 
   * `ducktape` uses `Transformers` as a user-controlled extension point of the library for transforming 
   * types that are not handled out of the box (or if the user wants to override the library-defined transformation).
@@ -56,10 +56,40 @@ object Transformer {
     }
   }
 
+  /**
+  * A `Transformer.Fallible[F, Source, Dest]` describes a fallible transformation from `Source` to `F[Dest]`.
+  * 
+  * `ducktape` uses `Transformer.Fallible` as a user-controlled extension point of the library for transforming 
+  * types that are not handled out of the box in fallible transformations (or if the user wants to override the library-defined transformation).
+  * 
+  * The recommended way of defining a `Transformer.Fallible` is a SAM lambda:
+  * {{{
+  * given Transformer.Fallible[[a] =>> Either[String, a], Int, String] = 
+  *   int => if int > 10 then Right(int.toString) else Left("lesser than 10 :(")
+  * }}}
+  */
   @FunctionalInterface
   trait Fallible[F[+x], Source, Dest] extends Fallible.Derived[F, Source, Dest]
 
   object Fallible {
+    /**
+    * `Transformer.Fallible.Derived[F, Source, Dest]` is a last resort mechanism for deriving transformations for types that are not known
+    * at definition site of the transformation, for example:
+    * {{{
+    * final case class Source[A](field1: Int, field2: String, generic: A)
+    * final case class Dest[A](field1: Int, field2: String, generic: A)
+    *
+    * def transformSource[F[+x], A, B](source: Source[A])(using Mode[F], Transformer.Derived[F, A, B]): F[Dest[B]] = 
+    *   source.fallibleTo[Dest[B]]
+    *
+    * transformSource[[a] =>> Either[String, a], Int, Option[Int]](Source(1, "2", 3))
+    * // Right(Dest(1, "2", Some(3)))
+    * }}}  
+    * 
+    * Instances of `Transformer.Fallible.Derived[Source, Dest]` are automatically derived but the derivation only kicks in as a last resort when requested by the user (like in the above example).
+    * 
+    * Users should NOT provide their own `Transformer.Fallible.Derived[F, Source, Dest]` instances - `Transformer.Fallible[F, Source, Dest]` is fit for exactly that purpose.
+    */
     sealed trait Derived[F[+x], Source, Dest] {
       def transform(source: Source): F[Dest]
     }
